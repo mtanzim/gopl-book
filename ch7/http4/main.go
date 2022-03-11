54dabe68fcf305fc05338131683d136af559f366
Tanzim Mokammel <mtanzim@gmail.com>
2022-03-10 20:11:22 -0500

    simple delete

--- ch7/http4/main.go
+++ ch7/http4/main.go
@@ -20,7 +20,7 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (db database) price(w http.ResponseWriter, req *http.Request) {
+func (db database) readPrice(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	price, ok := db[item]
 
@@ -32,9 +32,23 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	_, ok := db[item]
+
+	if !ok {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+	delete(db, item)
+	fmt.Fprintf(w, "Deleted %s\n", item)
+
+}
+
 func main() {
 	db := database{"shoes": 55, "tuxedo": 567}
 	http.HandleFunc("/list", db.list)
-	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/price", db.readPrice)
+	http.HandleFunc("/delete", db.delete)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
