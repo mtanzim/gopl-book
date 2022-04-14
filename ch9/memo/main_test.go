83bcd09ac6703d3395ffd5fcbad2a0606c9a4282
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-04-14 15:16:19 -0400

    add done channel to memo

--- ch9/memo/main_test.go
+++ ch9/memo/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -13,10 +14,25 @@ import (
 	// memo "github.com/mtanzim/gopl-book/ch9/memo/memo2"
 	// memo "github.com/mtanzim/gopl-book/ch9/memo/memo3"
 	// memo "github.com/mtanzim/gopl-book/ch9/memo/memo4"
-	memo "github.com/mtanzim/gopl-book/ch9/memo/memo5"
+	// memo "github.com/mtanzim/gopl-book/ch9/memo/memo5"
+	memo "github.com/mtanzim/gopl-book/ch9/memo/memo6"
 )
 
-func httpGetBody(url string) (interface{}, error) {
+func httpGetBody(url string, done chan struct{}) (interface{}, error) {
+
+	cancelled := func() bool {
+		select {
+		case <-done:
+			return true
+		default:
+			return false
+		}
+	}
+
+	if cancelled() {
+		return nil, errors.New("Execution cancelled")
+	}
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -40,10 +56,10 @@ func incomingUrls() []string {
 }
 
 func TestMainSequential(t *testing.T) {
-	m := memo.New(httpGetBody)
+	m := memo.New(httpGetBody, nil)
 	for _, url := range incomingUrls() {
 		start := time.Now()
-		value, err := m.Get(url)
+		value, err := m.Get(url, nil)
 		if err != nil {
 			log.Print(err)
 			continue
@@ -53,14 +69,34 @@ func TestMainSequential(t *testing.T) {
 }
 
 func TestMainConcurrent(t *testing.T) {
-	m := memo.New(httpGetBody)
+	m := memo.New(httpGetBody, nil)
 	var n sync.WaitGroup
 	for _, url := range incomingUrls() {
 		n.Add(1)
 		go func(url string) {
 			defer n.Done()
 			start := time.Now()
-			value, err := m.Get(url)
+			value, err := m.Get(url, nil)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s\t\t %s\t\t %d bytes\n", url, time.Since(start), len(value.([]byte)))
+		}(url)
+	}
+	n.Wait()
+}
+
+func TestMainConcurrentWithCancel(t *testing.T) {
+	done := make(chan struct{})
+	m := memo.New(httpGetBody, done)
+	var n sync.WaitGroup
+	for _, url := range incomingUrls() {
+		n.Add(1)
+		go func(url string) {
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url, done)
 			if err != nil {
 				log.Print(err)
 				return
---
+++ ch9/memo/memo6/memo.go
@@ -0,0 +1,73 @@
+package memo6
+
+import "fmt"
+
+type entry struct {
+	res   result
+	ready chan struct{} // closed when res is ready
+}
+
+type Func func(key string, done chan struct{}) (interface{}, error)
+type result struct {
+	value interface{}
+	err   error
+}
+
+type request struct {
+	key      string
+	response chan<- result
+	done     chan<- struct{}
+}
+
+type Memo struct {
+	requests chan request
+}
+
+func New(f Func, done chan struct{}) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f, done)
+	return memo
+}
+
+func (memo *Memo) server(f Func, done chan struct{}) {
+	cache := make(map[string]*entry)
+
+	for {
+		select {
+		case req := <-memo.requests:
+			e := cache[req.key]
+			// cache miss
+			if e == nil {
+				fmt.Println("cache miss for: " + req.key)
+				e = &entry{ready: make(chan struct{})}
+				cache[req.key] = e
+				go e.call(f, req.key, done)
+			}
+			go e.deliver(req.response)
+		case <-done:
+			close(memo.requests)
+			return
+		}
+	}
+}
+
+func (e *entry) deliver(response chan<- result) {
+	// wait for cache value to be ready
+	<-e.ready
+	// return data back to the channel
+	response <- e.res
+}
+
+func (e *entry) call(f Func, key string, done chan struct{}) {
+	e.res.value, e.res.err = f(key, done)
+	// broadcase that cache value is ready
+	close(e.ready)
+}
+
+// Non blocking cache with channels only
+func (memo *Memo) Get(key string, done chan struct{}) (interface{}, error) {
+	response := make(chan result)
+	memo.requests <- request{key, response, done}
+	res := <-response
+	return res.value, res.err
+}
